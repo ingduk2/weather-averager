15d8d3908ef8024ce0fb0b0b411974027b27152b
ingduk2 <ingduk2@naver.com>
2025-06-15 22:49:37 +0900

    chore: fix typo in statusCode

--- src/lib/error/error.ts
+++ src/lib/error/error.ts
@@ -1,9 +1,9 @@
 export class CustomError extends Error {
   statusCode: number;
 
-  constructor(message: string, statucCode: number = 500) {
+  constructor(message: string, statusCode: number = 500) {
     super(message);
-    this.statusCode = statucCode;
+    this.statusCode = statusCode;
   }
 }
 
